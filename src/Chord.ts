339476cfd4acdd9343a968c2c092b96737981f4b
Kai Kodama <cai.codama@gmail.com>
2019-07-26 20:00:12 +0900

    Add forms

--- src/Chord.ts
+++ src/Chord.ts
@@ -7,155 +7,75 @@ export default function detect(notes: Note[]): string {
   const noteNumbers = notes.map(n => noteToNumber(n));
   const baseNoteNumber = Math.min(...noteNumbers);
   const baseNote = noteFromNumber(baseNoteNumber);
-  const structure = {
-    a1: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 1),
-    a2: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 2),
-    a3: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 3),
-    a4: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 4),
-    a5: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 5),
-    a6: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 6),
-    a7: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 7),
-    a8: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 8),
-    a9: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 9),
-    a10: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 10),
-    a11: noteNumbers.some(n => (n - baseNoteNumber) % 12 === 11),
-  };
-
-  const estimated = [
-    {
-      label: "",
-      score: estimate(structure, major),
-    },
-    {
-      label: "m",
-      score: estimate(structure, minor),
-    },
-    {
-      label: "aug",
-      score: estimate(structure, augment),
-    },
-    {
-      label: "dim",
-      score: estimate(structure, diminish),
-    },
-    {
-      label: "sus2",
-      score: estimate(structure, sus2),
-    },
-    {
-      label: "sus4",
-      score: estimate(structure, sus4),
-    },
-    {
-      label: "M7",
-      score: estimate(structure, major7th),
-    },
-    {
-      label: "M9",
-      score: estimate(structure, major9th),
-    },
-    {
-      label: "m7",
-      score: estimate(structure, minor7th),
-    },
-    {
-      label: "m9",
-      score: estimate(structure, minor9th),
-    },
-  ];
-
-  if (estimated.every(e => e.score < 0.5)) {
-    return `?/${baseNote.key}`;
+  const first = detectOnce(notes);
+  const second = detectOnce(notes.filter(n => n.key !== baseNote.key));
+  if (first.score < second.score) {
+    return `${second.key}${second.label}/${baseNote.key} (${Math.ceil(
+      second.score * 100,
+    )}%)`;
   }
-  const max = estimated.reduce((x, y) => (x.score >= y.score ? x : y));
-  return `${baseNote.key}${max.label}`;
+  return `${baseNote.key}${first.label} (${Math.ceil(first.score * 100)}%)`;
 }
 
-interface Structure {
-  a1?: boolean;
-  a2?: boolean;
-  a3?: boolean;
-  a4?: boolean;
-  a5?: boolean;
-  a6?: boolean;
-  a7?: boolean;
-  a8?: boolean;
-  a9?: boolean;
-  a10?: boolean;
-  a11?: boolean;
+function detectOnce(notes: Note[]) {
+  const noteNumbers = notes.map(n => noteToNumber(n));
+  const baseNoteNumber = Math.min(...noteNumbers);
+  const baseNote = noteFromNumber(baseNoteNumber);
+  const structure = Array(11)
+    .fill(null)
+    .map((_, idx) => (idx + 1) as FormElement)
+    .filter(distance => {
+      return noteNumbers.some(n => (n - baseNoteNumber) % 12 === distance);
+    });
+
+  const estimated = (Object.keys(patterns) as (keyof typeof patterns)[]).map(
+    key => ({
+      label: key,
+      score: estimate(structure, patterns[key]),
+    }),
+  );
+
+  return {
+    key: baseNote.key,
+    ...estimated.reduce((x, y) => (x.score >= y.score ? x : y)),
+  };
 }
 
-const major: Structure = {
-  a4: true,
-  a7: true,
+type FormElement = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
+type Form = FormElement[];
+
+const patterns: { [key: string]: Form } = {
+  "5": [7],
+  "": [4, 7],
+  m: [3, 7],
+  aug: [4, 8],
+  dim: [3, 6],
+  sus2: [2, 7],
+  sus4: [5, 7],
+  M7: [4, 7, 11],
+  m7: [3, 7, 10],
+  "7": [4, 7, 10],
+  "7♭5": [4, 6, 10],
+  "7sus4": [5, 7, 10],
+  mM7: [3, 7, 11],
+  dim7: [3, 6, 11],
+  ø7: [3, 6, 10],
+  M9: [2, 4, 7, 11],
+  m9: [2, 3, 7, 10],
+  M11: [2, 4, 5, 7, 11],
+  m11: [2, 3, 5, 7, 10],
+  M13: [2, 4, 6, 7, 9, 11],
+  m13: [2, 3, 5, 7, 9, 10],
 };
 
-const minor: Structure = {
-  a3: true,
-  a7: true,
-};
-
-const augment: Structure = {
-  a4: true,
-  a8: true,
-};
-
-const diminish: Structure = {
-  a3: true,
-  a6: true,
-};
-
-const sus2: Structure = {
-  a2: true,
-  a7: true,
-};
-
-const sus4: Structure = {
-  a5: true,
-  a7: true,
-};
-
-const major7th: Structure = {
-  a4: true,
-  a7: true,
-  a11: true,
-};
-
-const major9th: Structure = {
-  a2: true,
-  a4: true,
-  a7: true,
-  a11: true,
-};
-
-const minor7th: Structure = {
-  a3: true,
-  a7: true,
-  a10: true,
-};
-
-const minor9th: Structure = {
-  a2: true,
-  a3: true,
-  a7: true,
-  a10: true,
-};
-
-function estimate(structure: Structure, chord: Structure): number {
-  return Object.keys(structure)
-    .map(key => {
-      if (structure[key as keyof Structure]) {
-        if (chord[key as keyof Structure])
-          return (
-            1 /
-            Object.keys(structure).filter(k => chord[k as keyof Structure])
-              .length
-          );
-        return (
-          -1 /
-          Object.keys(structure).filter(k => !chord[k as keyof Structure])
-            .length
-        );
+function estimate(structure: Form, chord: Form): number {
+  const allFormElements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] as Form;
+  return allFormElements
+    .map(distance => {
+      if (structure.includes(distance)) {
+        if (chord.includes(distance))
+          return 1 / allFormElements.filter(k => chord.includes(k)).length;
+        return -1 / allFormElements.filter(k => !chord.includes(k)).length;
       }
       return 0;
     })
