ba36e99e54ed59d5e0c0ecdf9d095555df626048
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-13 01:45:56 +0000

    Deploy kdama/piano to github.com/kdama/piano.git:gh-pages

---
+++ precache-manifest.565523a3fd2b9ee0758eefbf1da3363a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbe2fb543a3b291126703daafccc34e",
+    "url": "/piano/index.html"
+  },
+  {
+    "revision": "0ab50187e1d9d7998870",
+    "url": "/piano/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "82588f365bcf9792ba14",
+    "url": "/piano/static/js/2.4e3f4806.chunk.js"
+  },
+  {
+    "revision": "0ab50187e1d9d7998870",
+    "url": "/piano/static/js/main.8757c6d6.chunk.js"
+  },
+  {
+    "revision": "ef83fb9f0730b13f09a0",
+    "url": "/piano/static/js/runtime-main.90f4024e.js"
+  }
+]);
